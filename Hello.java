0d5defe7357ef6d7549ca5e03ba7a0b4af4f2221
awellsbiz <awellsbiz@gmail.com>
2024-01-21 11:32:26 -0800

    operators review

--- Hello.java
+++ Hello.java
@@ -12,13 +12,20 @@
 
 class Hello
 {
-    public static void main(String a[])
+    public static void main(String args[])
       {
-        int num = 3;
-        int num2 = 5;
-        int result = num * num2;
+        // int num = 3;
+        // int num2 = 5;
+        // int result = num * num2;
 
-        System.out.println(result);
+        // System.out.println(result);
+
+        //byte b = 127;
+        int a = 257;
+        byte k = (byte) a;
+
+
+        System.out.println(k);
         
       }
 
@@ -37,14 +44,15 @@ class Hello
 
     //1. integer- has different sizes. 1 int is equal to 4 bytes. long is 8 bytes. if you want to save space you go for short whitch is 2 bytes. a byte is a byte.
         // byte, short, ints, long( long needs and l flag after the number)
+        //byte is used when you want to save memory or when dealing woth a stream of data from a file or network. Arithmetic operations on byte will automatically be stored as int. 
 
     //2. float- only takes 4 bytes. double takes 8 bytes- float is actually 8 bytes
         //double, float
         //to define that it is a float you need an f flag at the end -- float num = 6.7f - double is default when using float type. 
 
-    {
-      float marks = 6.7
-    }
+    // {
+    //   float marks = 6.7
+    // }
 
     //3. characters - is 2 bytes -- java supports unicode 
       //char c = 'k';
@@ -62,6 +70,26 @@ class Hello
     // char c = 'a' -> c++ -> Ststem.out.print(c) -> b
 
   //:::::Type Conversion and Casting :::::
+
+    // cant change a type of a variable 
+    // casting is -explicit conversion
+
+    // if i wanted to store  byte into int
+
+      // byte b = 127 -- byte goes only up to 127- cant store more than that
+
+      // int a = 256
+
+        // b = a --will not work- because you are narrowing it since bytes are smaller than integers. you cant put a bigger item in a small box. cannot change an integer into a byte format
+
+        // a = b --will work because e are widening it. you can put a smaller item in a bigger box. this is implicit conversion.
+
+      // you can give a conversion if you really wanted to convert a byte into an integer:
+        // b = (byte)a - this is casting- this is expicit
+
+      
+
+
 
 
     
---
+++ Operators.java
@@ -0,0 +1,25 @@
+class Operators 
+{
+  public static void main ( String args[] ) 
+  {
+    int num = 9;
+    //int num2 = 5;
+
+    // int result = num1 % num2;
+
+   // num1--; decrement 
+   // num1++; // post increment
+   //++num1; // pre increment
+   // num1 *= 2; // multiplication
+   //num1 /= 2; // dvivision- dosnt give the remainder
+   //num1 %= 2; //modular
+
+   // int result = num++; //first it will fetch the value then increment and make it 10
+
+   //int result = ++num // it will increment then fetch the value
+
+
+
+    System.out.println(result);
+  }
+}
