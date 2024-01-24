bec6fd167fc2dc9ea4a6cac8c781ef9630f89a3c
awellsbiz <awellsbiz@gmail.com>
2024-01-23 23:22:29 -0800

    enums

--- Interface.java
+++ Interface.java
@@ -1,4 +1,6 @@
 interface Computer {
+  int age = 66; // final and static
+
   public void code();
 }
 
@@ -20,6 +22,14 @@ class Developer {
   }
 }
 
+class C implements Computer // with implements you only get methods not variables. thats why its static and
+                            // finaal beacuse interface dosent have own memory in heap.
+{
+  public void code() {
+    System.out.println("from C ");
+  }
+}
+
 public class Interface {
 
   public static void main(String a[]) {
@@ -28,6 +38,12 @@ public class Interface {
 
     Developer ajay = new Developer();
     ajay.devApp(desk);
+
+    Computer mac;
+
+    mac = new C();
+    mac.code();
+
   }
 }
 
@@ -37,3 +53,36 @@ public class Interface {
 // its not a class- cannot instanciate it
 // create an interface for a design- gives the path/ directions- tell us (
 // classes ) the methods we need
+
+// class -> class = extendte
+// class -interface -> implements
+// interfact - interface -> extends
+
+// you can implement more than one interfactes with a class ::: implements
+// y,c::::
+
+// ::::Enums ::::::
+
+// enum Status
+// {
+// running, Failed, Pending, Success;
+// }
+
+// public class Demo
+// {
+// public static void main( String a [])
+// [
+// int i = 5;
+// Status s = Status.Running; //prints running
+
+// Status[] ss = Status.value();
+
+// for (Status s : ss)
+// {
+// System.out.println(s);
+// }
+// ]
+// }
+
+// you can pass arguemnets with the objevts in the enums class. then create a
+// constructor.
