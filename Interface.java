a26dd5929e4ea217967f50cf69e8f3f4f1040a91
awellsbiz <awellsbiz@gmail.com>
2024-01-27 11:15:24 -0800

    update

--- Interface.java
+++ Interface.java
@@ -1,51 +1,51 @@
-interface Computer {
-  int age = 66; // final and static
+// interface Computer {
+//   int age = 66; // final and static
 
-  public void code();
-}
+//   public void code();
+// }
 
-class Laptop implements Computer {
-  public void code() {
-    System.out.println("code compile run in laptop ");
-  }
-}
+// class Laptop implements Computer {
+//   public void code() {
+//     System.out.println("code compile run in laptop ");
+//   }
+// }
 
-class Desktop implements Computer {
-  public void code() {
-    System.out.println("code compile run in desktip");
-  }
-}
+// class Desktop implements Computer {
+//   public void code() {
+//     System.out.println("code compile run in desktip");
+//   }
+// }
 
-class Developer {
-  public void devApp(Computer lap) {
-    lap.code();
-  }
-}
+// class Developer {
+//   public void devApp(Computer lap) {
+//     lap.code();
+//   }
+// }
 
-class C implements Computer // with implements you only get methods not variables. thats why its static and
-                            // finaal beacuse interface dosent have own memory in heap.
-{
-  public void code() {
-    System.out.println("from C ");
-  }
-}
+// class C implements Computer // with implements you only get methods not variables. thats why its static and
+//                             // finaal beacuse interface dosent have own memory in heap.
+// {
+//   public void code() {
+//     System.out.println("from C ");
+//   }
+// }
 
-public class Interface {
+// public class Interface {
 
-  public static void main(String a[]) {
-    Computer lap = new Laptop();
-    Computer desk = new Desktop();
+//   public static void main(String a[]) {
+//     Computer lap = new Laptop();
+//     Computer desk = new Desktop();
 
-    Developer ajay = new Developer();
-    ajay.devApp(desk);
+//     Developer ajay = new Developer();
+//     ajay.devApp(desk);
 
-    Computer mac;
+//     Computer mac;
 
-    mac = new C();
-    mac.code();
+//     mac = new C();
+//     mac.code();
 
-  }
-}
+//   }
+//}
 
 // instead of creating an abstract class you can create an Interface- by default
 // its public abstract- like a class but by default all methoids ar public
@@ -86,3 +86,49 @@ public class Interface {
 
 // you can pass arguemnets with the objevts in the enums class. then create a
 // constructor.
+
+// Lambda expression
+// @FunctionalInterface
+// interface a{
+// void show()
+// }
+// public class Demo{
+
+// publick static void main (String a [])
+// {
+// //A obj = () -> System.out.println("...");
+// obj.show()
+// }
+// }
+
+// lambda expression only works with a functional interface
+
+// Diufferent type of interfaces(only declare methods automically public
+// abstract ):
+
+// 1.Normal- multiple methods
+
+// 2. functiona interface /SAM -Single Abstract Method-
+
+// 3. Marks- blank interface- why? to update something to the compiler.
+// Civilaization you can take a obj and it had diff variables if you can save in
+// hard drive. Deserilization you can import thos saved values into the heap
+// when its called back ( re-complied again )
+
+// :: Different errors ::
+
+// 1. complie time error
+// 2. runtime error - execution normally stops. -> exception and need to handle
+// the exceptions- is a try catch block- you can have more than one catch block ad be specific with Exception class like (ArithmicticException e ). Excetion is a parent class - put st end
+
+//inside of a catch block you can handle exception like calling another datatbsase or setingthe variable to another value or whatever. 
+// 3.logical errror - testing- checking inputs and out puts.
+
+//use extends Exeption to create your own exception. 
+//use super to pass a an argument. 
+
+public class Interface {
+  public static void main(String a[]) {
+    int i = 9;
+  }
+}
